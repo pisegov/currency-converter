406a57049b1a172cf03effac912ec4530e710966
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2024-07-22 14:49:17 +0300

    move decimal formatter logic to use case and replace magic numbers and characters with constants

--- features/converter/src/main/java/com/myaxa/converter/domain/usecase/FormatDecimalStringUseCase.kt
+++ features/converter/src/main/java/com/myaxa/converter/domain/usecase/FormatDecimalStringUseCase.kt
@@ -1,12 +1,45 @@
 package com.myaxa.converter.domain.usecase
 
-import com.myaxa.converter.domain.DecimalFormatter
 import javax.inject.Inject
 
-internal class FormatDecimalStringUseCase @Inject constructor(
-    private val decimalFormatter: DecimalFormatter,
-) {
+internal class FormatDecimalStringUseCase @Inject constructor() {
+
+    companion object {
+        private const val POINT_CHARACTER = '.'
+        private const val COMMA_CHARACTER = ','
+        private const val DECIMAL_PLACES_OFFSET = 3
+        private const val STRING_START = 0
+    }
+
     operator fun invoke(string: String): String {
-        return decimalFormatter.format(string)
+        return format(string)
+    }
+
+    private fun format(s: String): String {
+        val string = s
+            .trim()
+            .replace(COMMA_CHARACTER, POINT_CHARACTER)
+            .filterMultiplePoints()
+
+        val pointIndex = string.indexOf(POINT_CHARACTER)
+        if (pointIndex < 0) return string
+
+        return if (pointIndex + DECIMAL_PLACES_OFFSET < string.length) {
+            string.substring(STRING_START, pointIndex + DECIMAL_PLACES_OFFSET)
+        } else {
+            string
+        }
+    }
+
+    private fun String.filterMultiplePoints(): String {
+        var hasPoint = false
+        return filter {
+            val itIsPoint = it == POINT_CHARACTER
+            if (hasPoint && itIsPoint) return@filter false
+
+            if (itIsPoint) hasPoint = true
+
+            true
+        }
     }
 }
